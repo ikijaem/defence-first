e6b9228aa9b98da76a0e8201bc0f2aa8fcd5e7d9
ikijaem <ilija.malinovic@yahoo.com>
2020-05-29 21:05:17 +0200

    initial commit

---
+++ defence-first/src/main/java/bsep/defence_first/App.java
@@ -0,0 +1,12 @@
+package bsep.defence_first;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App 
+{
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+}
